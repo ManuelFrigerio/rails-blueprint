b16af9af1c394f3c7af1560b9aa5e4fa11df49f5
Manuel Frigerio <manuel.frigerio@gmail.com>
2018-10-30 12:44:23 +0100

    Updated Metatags

--- config/initializers/metatags.rb
+++ config/initializers/metatags.rb
@@ -1,8 +1,8 @@
 Metatags = {
-  title: "ReferralHero",
-  description: "Results-driven referral software for SaaS companies.",
-  website: "https://referralhero.app",
-  default_image: "/og_image.png",
-  twitter_handle: "referralheroHQ",
-  google_webmaster: "r2JkFHXGVneeX0JRnPoxeOMOnVbt5mPXJshDxLEO_dg"
+  title: "MyWebApp",
+  description: "Your description",
+  website: "https://mywebsite.com",
+  default_image: "/image.png",
+  twitter_handle: "mnlfrgr",
+  google_webmaster: "XXYYY"
 }
