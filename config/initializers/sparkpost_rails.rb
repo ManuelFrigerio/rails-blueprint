2f6fb1283072099829ca3f3db7203b11b6f3841c
Manuel Frigerio <manuel.frigerio@gmail.com>
2018-11-06 11:35:56 +0100

    Improved SparkPost file

--- config/initializers/sparkpost_rails.rb
+++ config/initializers/sparkpost_rails.rb
@@ -1,10 +1,9 @@
 SparkPostRails.configure do |c|
-  c.api_key = 'YOUR API KEY'
-  c.sandbox = true                                # default: false
+  c.api_key = ENV['SPARKPOST_API']
+  c.sandbox = false                               # default: false
   c.track_opens = true                            # default: false
   c.track_clicks = true                           # default: false
   c.transactional = true                          # default: false
   c.inline_css = true                             # default: false
   c.html_content_only = true                      # default: false
-  c.subaccount = "123"                            # default: nil
 end
