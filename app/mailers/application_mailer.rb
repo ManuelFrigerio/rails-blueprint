95963124d090064869ac0c6e72325ab3ca6d3463
Manuel Frigerio <manuel.frigerio@gmail.com>
2018-11-25 17:07:37 +0100

    Created application_mailer.rb file

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,19 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
+
+  append_view_path Rails.root.join('app', 'views', 'mailers')
+  default from: "#{ENV['APP_NAME']} <#{ENV['DEFAULT_EMAIL']}>"
+
+  private
+
+  def send_email(email, subject)
+    begin
+      mail(
+        to: email,
+        subject: subject
+      )
+    rescue => e
+      raise "Email error: #{e}"
+    end
+  end 
+
 end
