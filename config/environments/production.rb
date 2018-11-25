0b4d0fd1cf9449c0b09b25a3cb2545736d96332b
Manuel Frigerio <manuel.frigerio@gmail.com>
2018-11-25 16:48:49 +0100

    Use environment variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  config.action_mailer.default_url_options = { :host => 'https://mywebsite.com' }
+  config.action_mailer.default_url_options = { :host => ENV['APP_URL'] }
   config.action_mailer.delivery_method = :sparkpost
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
--- config/initializers/metatags.rb
+++
@@ -1,8 +0,0 @@
-Metatags = {
-  title: "MyWebApp",
-  description: "Your description",
-  website: "https://mywebsite.com",
-  default_image: "/image.png",
-  twitter_handle: "mnlfrgr",
-  google_webmaster: "XXYYY"
-}
