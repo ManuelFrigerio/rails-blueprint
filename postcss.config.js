5f8811dde8056a2caf8905b7ce9fc2e1e37bda32
Manuel Frigerio <manuel.frigerio@gmail.com>
2020-04-04 19:32:22 +0100

    Added TailwindCSS and PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+let environment = {
   plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
@@ -7,6 +9,24 @@ module.exports = {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
+    }),
   ]
 }
+
+// Only run PurgeCSS in production (you can also add staging here)
+if (process.env.RAILS_ENV === "production") {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/*.html.erb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/javascript/**/*.vue',
+        './app/javascript/**/*.jsx',
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
+}
+
+module.exports = environment
