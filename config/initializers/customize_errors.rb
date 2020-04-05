854155df0adb08f76d8c55f333e1060efdc2d36c
Manuel Frigerio <manuel.frigerio@gmail.com>
2020-04-05 19:07:02 +0100

    updated error class

--- config/initializers/customize_errors.rb
+++ config/initializers/customize_errors.rb
@@ -1,7 +1,7 @@
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   class_attr_index = html_tag.index 'class="'
 
-  error_class = "is-danger"
+  error_class = "border-red-500"
 
   if class_attr_index
     html_tag.insert class_attr_index+7, "#{error_class} "
