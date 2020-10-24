0ccef8e9dc150a12298af653f1fefe8bd24f972d
Manuel Frigerio <manuel.frigerio@gmail.com>
2020-10-24 17:05:39 +0100

    Added configurations to spec_helper.rb file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,6 +44,17 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
